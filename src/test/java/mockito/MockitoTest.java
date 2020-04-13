74e4c10541f17d84a77c2f2d97d53d4b3cfdd282
Kunal Jadhav <iamkunalhere@gmail.com>
2020-04-14 02:57:41 +0530

    ADDED: Example for mockito test 2

--- src/test/java/mockito/MockitoTest.java
+++ src/test/java/mockito/MockitoTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class MockitoTest {
@@ -22,5 +23,16 @@ public class MockitoTest {
         when(databaseMock.query("* from t")).thenReturn(true);
         boolean check = t.query("* from t");
         Assert.assertTrue(check);
+    }
+
+    @Test
+    public void customerReaderTest() {
+        Customer sampleCustomer = new Customer("Susan", "Ivanova");
+        EntityManager entityManager = mock(EntityManager.class);
+        when(entityManager.find(Customer.class,1L)).thenReturn(sampleCustomer);
+        CustomerReader customerReader = new CustomerReader();
+        customerReader.setEntityManager(entityManager);
+        String fullName = customerReader.findFullName(1L);
+        Assert.assertEquals("Susan Ivanova",fullName);
     }
 }
